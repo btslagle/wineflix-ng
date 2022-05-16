70e5b2afb2ba73841f328dad455e1eb61b6f0843
Brad Slagle <btslagle@gmail.com>
2022-05-16 17:09:08 -0500

    Attempted looping

--- src/app/keep-drinking/keep-drinking.component.ts
+++ src/app/keep-drinking/keep-drinking.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-
+import { wines } from "../data";
 @Component({
   selector: 'app-keep-drinking',
   templateUrl: './keep-drinking.component.html',
   styleUrls: ['./keep-drinking.component.css']
 })
 export class KeepDrinkingComponent  {
-
+  wines = wines
   
 }
